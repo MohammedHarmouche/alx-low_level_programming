deaa2766ecfb6ca1911b8eed5ca0a99dc978c33a
Mohammed Harmouche <mohammedharmouche1@gmail.com>
2023-03-16 20:30:58 +0100

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* Main - entery point
+*
+*Return: always 0 (success)
+*/
+main()
+{
+	printf("sizeof a char is : %d - byte\n", sizeof(char));
+	printf("sizeof an int is : %d - byte\n", sizeof(int));
+	printf("sizeof a float is : %d - byte\n", sizeof(float));
+	printf("sizeof a long int is : %d - byte\n", sizeof(long int));
+	printf("sizeof a long long int is : %d - byte\n", sizeof(long long int));
+	return (0);
+}
