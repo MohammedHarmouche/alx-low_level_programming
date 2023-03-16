c62297402b6c2a042c522f47fc57bb0666def40d
Mohammed Harmouche <mohammedharmouche1@gmail.com>
2023-03-16 20:51:50 +0100

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
 *
 *Return: always 0 (success)
 */
-int main()
+int main(void)
 {
 	printf("sizeof a char is : %d - byte(s)\n", sizeof(char));
 	printf("sizeof an int is : %d - byte(s)\n", sizeof(int));
