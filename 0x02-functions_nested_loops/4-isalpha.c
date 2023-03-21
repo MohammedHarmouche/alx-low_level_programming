3701d7aa7d69f186d27c5c229fc0340dfe8cc69c
Mohammed Harmouche <Mohammedharmouche1@gmail.com>
2023-03-21 19:09:54 +0000

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+   * _isalpha - check if the given character is lower case.
+     *
+      * Return: 1 (True) else 0 (False)
+       */
+int _isalpha(int c)
+{
+
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 98))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
