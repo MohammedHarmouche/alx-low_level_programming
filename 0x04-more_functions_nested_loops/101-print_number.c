f47c1e5272a84dbad92b654f52dce826e0625ea5
Mohammed Harmouche <Mohammedharmouche1@gmail.com>
2023-03-23 22:26:13 +0000

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,80 +1,38 @@
 #include "main.h"
 
-int _pow(int a, int b);
-
 /**
-   * print_number - print the given number.
-    * @n: number to print.
+   * print_number - print an integer, without using long, arrays, or pointers
+    * @n: number to be printed
      */
+
 void print_number(int n)
 {
-	int collection = n;
-	unsigned int new_num;
-	int count = 0;
-	int print = 0;
+	unsigned int tens, digit, positive = n;
+	double t_beg = 1;
 
-	if (n < 0)
+	if (n == 0)
 	{
-		new_num = -n;
-		_putchar('-');
+		_putchar('0');
 	}
 	else
 	{
-		new_num = n;
-	}
+		if (n < 0)
+		{
+			positive = n * -1;
+			_putchar('-');
+		}
+		while (t_beg <= positive)
+		{
+			t_beg *= 10;
+		}
+		tens = t_beg / 10;
 
-	while (collection != 0)
-	{
-		collection /= 10;
-		++count;
+		while (tens >= 1)
+		{
+			digit = positive / tens;
+			_putchar(digit + '0');
+			positive = (positive - (tens * digit));
+			tens /= 10;
+		}
 	}
-	count -= 1;
-	do {
-		if ((new_num / (_pow(10, count)) > 9) && count > 1)
-		{
-			print = new_num / (_pow(10, count));
-			print = print % 10;
-			_putchar('0' + print);
-		}
-		else if (count > 1)
-		{
-			print = (new_num / _pow(10, count));
-		       _putchar('0' + print);
-		}
-		else if (((new_num / 10) > 9) && count == 1)
-		{
-			print = (new_num / 10) % 10;
-			_putchar('0' + print);
-		}
-		else if (count == 1)
-		{
-			print = new_num / 10;
-			_putchar('0' + print);
-		}
-		else
-		{
-			print = new_num % 10;
-			_putchar('0' + print);
-		}
-		count--;
-	} while (count > -1);
-}
-
-/**
-   * _pow - power fuction
-    * @a: value to use.
-     * @b: growth count
-      *
-       * Return: value int.
-	*/
-int _pow(int a, int b)
-{
-	int i;
-	int p = a;
-
-	for (i = 1 ; i < b ; i++)
-	{
-		a *= p;
-	}
-	return (a);
 }
