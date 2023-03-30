788af4ae9acd9807f520cf10b9ba532542c129f1
Mohammed Harmouche <Mohammedharmouche1@gmail.com>
2023-03-30 17:01:36 +0000

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,9 +3,8 @@
    * reverse_array - reverse array of integers
     * @a: array
      * @n: number of elements of array
-      *
        * Return: void
-        */
+	*/
 void reverse_array(int *a, int n)
 {
 	int i;
