e614996ccb60f9ad290500f9e642eddfc40465b3
Mohammed Harmouche <Mohammedharmouche1@gmail.com>
2023-04-05 13:22:47 +0000

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+   *_pow_recursion- power of int
+    *@x: int to be powerd
+     *@y: int to be raised
+      *
+       *Return: result
+        */
+int _pow_recursion(int x, int y)
+{
+	int result = x;
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if  (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		result *= _pow_recursion(x, y - 1);
+	}
+	return (result);
+}
