3c5370cabede854a91bbdf38c3809961c03040dc
Mohammed Harmouche <Mohammedharmouche1@gmail.com>
2023-04-08 17:29:55 +0000

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+   * main - prints name of file.
+    * @argc: size of argv
+     * @argv: arguments entered
+       * Return: 0
+        */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	argc = argc;
+
+	return (0);
+}
