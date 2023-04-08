297cfe45e836707be78f1641ca0348c7a9b0730c
Mohammed Harmouche <Mohammedharmouche1@gmail.com>
2023-04-08 20:57:05 +0000

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+   *main- prints the number of argument entred
+    *@argc: the number of arguments
+     *@argv: arguments entered
+       *Return: 0
+	*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	argv = argv;
+
+	return (0);
+}
