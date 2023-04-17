3f52fc31a231888f61ab813d785129dd6f92b543
Mohammed Harmouche <Mohammedharmouche1@gmail.com>
2023-04-17 16:34:31 +0000

    Task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,10 +7,11 @@
      * @name: dog's name.
       * @age: dog's age.
        * @owner: dog's owner.
- 	*/
+	*/
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if(d != NULL)	
+	if (d != NULL)
 	{
 		d->name = name;
 		d->age = age;
