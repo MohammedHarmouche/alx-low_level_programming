efd7be72b7073cd98722cb26db9a4401c27546dd
Mohammed Harmouche <Mohammedharmouche1@gmail.com>
2023-04-30 12:32:51 +0100

    Updated

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,14 +1,12 @@
-#ifndef LISTS
-#define LISTS
-
-#include <stdio.h>
+#ifndef LISTS_H
+#define LISTS_H
 #include <stdlib.h>
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- */
+   * struct listint_s - singly linked list
+    * @i: integer
+      * @next: points to the next node
+	*/
 typedef struct listint_s
 {
 	int n;
@@ -17,11 +15,10 @@ typedef struct listint_s
 
 /**
  * struct listp_s - singly linked list
- * @p: pointers of nodes
- * @next: points to the next node
- *
- * Description: singly linked list of pointers
- */
+  * @p: pointers of nodes
+   * @next: points to the next node
+     * Description: singly linked list of pointers
+      */
 typedef struct listp_s
 {
 	void *p;
