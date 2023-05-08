287d75aa6d898298e2be6cb7aed043f4c4757f09
Mohammed Harmouche <Mohammedharmouche1@gmail.com>
2023-05-08 11:08:05 +0100

    Task 1

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -10,6 +9,7 @@
 
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 
 #endif
