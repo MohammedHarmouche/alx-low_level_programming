8ae957e5542660e8af5f4432c2c001f06ac3b0a0
Mohammed Harmouche <Mohammedharmouche1@gmail.com>
2023-05-08 11:09:10 +0100

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,7 +5,7 @@
      * @filename: name of the file to create.
       * @text_content: text to add to the new file.
        * Return: Always 1 on scucess, -1 on Failure
- 	*/
+	*/
 
 int create_file(const char *filename, char *text_content)
 {
